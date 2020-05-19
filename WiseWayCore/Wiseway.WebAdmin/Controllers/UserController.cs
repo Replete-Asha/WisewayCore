e9cb93430080b3dc745754cfc64746d565687f90
asha <asha.shete@repleteinc.com>
2020-05-19 20:29:21 +0530

    added login page for webadmin

--- WiseWayCore/Wiseway.WebAdmin/Controllers/UserController.cs
+++ WiseWayCore/Wiseway.WebAdmin/Controllers/UserController.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WiseWay.Core;
+using WiseWay.Services;
 
 namespace Wiseway.WebAdmin.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult SignIn()
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public IActionResult LogIn()
         {
             return View();
+        }
+
+        [TempData]
+        public string SessionUserId { get; set; }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Authenticate(User model)
+        {
+            var loginUser = _userService.Authenticate(model);
+
+            if (loginUser == null)
+                return View(loginUser);
+
+            SessionUserId = JsonConvert.SerializeObject(loginUser.Id);
+            TempData.Keep("SessionUserId");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- WiseWayCore/Wiseway.WebAdmin/Startup.cs
+++ WiseWayCore/Wiseway.WebAdmin/Startup.cs
@@ -1,13 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using System.IO;
+using System.Text;
+using WiseWay.Core;
+using WiseWay.Services;
 
 namespace Wiseway.WebAdmin
 {
@@ -24,6 +25,33 @@ namespace Wiseway.WebAdmin
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,7 +95,7 @@ namespace Wiseway.WebAdmin
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "Home", action = "Index" });
+                    defaults: new { controller = "User", action = "LogIn" });
 
                 endpoints.MapControllerRoute(
                     name: "api",
